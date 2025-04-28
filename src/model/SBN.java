1483fbdae5938280f0a14c06d60a331ee44dca24
BayuMahardika <114842921+Bayu-cyber-coder@users.noreply.github.com>
2025-04-28 23:56:07 +0800

    Update SBN.java

--- src/model/SBN.java
+++ src/model/SBN.java
@@ -2,15 +2,17 @@ package model;
 
 public class SBN {
     private String nama;
-    private double bunga; // persen per tahun
-    private int jangkaWaktu; // tahun
+    private double bunga;
+    private int jangkaWaktu;
     private String jatuhTempo;
+    private double kuotaNasional;
 
-    public SBN(String nama, double bunga, int jangkaWaktu, String jatuhTempo) {
+    public SBN(String nama, double bunga, int jangkaWaktu, String jatuhTempo, double kuotaNasional) {
         this.nama = nama;
         this.bunga = bunga;
         this.jangkaWaktu = jangkaWaktu;
         this.jatuhTempo = jatuhTempo;
+        this.kuotaNasional = kuotaNasional;
     }
 
     public String getNama() {
@@ -27,5 +29,13 @@ public class SBN {
 
     public String getJatuhTempo() {
         return jatuhTempo;
+    }
+
+    public double getKuotaNasional() {
+        return kuotaNasional;
+    }
+
+    public void setKuotaNasional(double kuotaNasional) {
+        this.kuotaNasional = kuotaNasional;
     }
 }
