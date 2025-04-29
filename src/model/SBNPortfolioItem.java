b2318b026e7146f4ad8633ca5145acf15692b574
BayuMahardika <114842921+Bayu-cyber-coder@users.noreply.github.com>
2025-04-29 15:36:21 +0800

    Update SBNPortfolioItem.java

--- src/model/SBNPortfolioItem.java
+++ src/model/SBNPortfolioItem.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class SBNPortfolioItem {
     private SBN sbn;
     private double nominal;
@@ -19,8 +22,20 @@ public class SBNPortfolioItem {
 
     @Override
     public String toString() {
+        System.out.println("+-------------------------------------------------------------------------------------------------------------+");
+        System.out.println("| Nama SBN                       |        Nominal  |    Kupon/bln    | Bunga    | Jangka      | Jatuh Tempo   |");
+        System.out.println("+-------------------------------------------------------------------------------------------------------------+");
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("id", "ID"));
         double bungaPerBulan = sbn.getBunga() / 12 / 100 * nominal;
-        return sbn.getNama() + " | nominal: " + nominal +
-                " | kupon/bln: " + bungaPerBulan;
+        return String.format(
+                "| %-30s | %15s | %15s | %6.2f%% | %6d tahun | %-12s |",
+                sbn.getNama(),
+                formatter.format(nominal),
+                formatter.format(bungaPerBulan),
+                sbn.getBunga(),
+                sbn.getJangkaWaktu(),
+                sbn.getJatuhTempo()
+        );
     }
 }
+
