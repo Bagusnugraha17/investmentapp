2fde958428a23f5c7aa107ffd9dee3bf2e1fef5b
BayuMahardika <114842921+Bayu-cyber-coder@users.noreply.github.com>
2025-04-29 15:33:08 +0800

    ganti user dan pass

--- src/util/LoginUtil.java
+++ src/util/LoginUtil.java
@@ -6,10 +6,10 @@ import model.User;
 
 public class LoginUtil {
     public static User authenticate(String user, String pass) {
-        if ("Bayusuputra24117".equals(user) && "bayuganteng2".equals(pass)) {
+        if ("admin".equals(user) && "admin01".equals(pass)) {
             return new Admin(user, pass);
         }
-        if ("Bagusnugraha24139".equals(user) && "tugus123".equals(pass)) {
+        if ("customer".equals(user) && "cust01".equals(pass)) {
             return new Customer(user, pass);
         }
         return null;
